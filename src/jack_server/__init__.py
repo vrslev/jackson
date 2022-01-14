fea05d4e29ae3de31b824275055e587882af1ea2
Lev Vereshchagin <mail@vrslev.com>
2022-01-14 21:54:46 +0300

    Export set_error_function and set_info_function from jack_server

--- src/jack_server/__init__.py
+++ src/jack_server/__init__.py
@@ -1,3 +1,5 @@
 from jack_server._server import Driver as Driver
 from jack_server._server import Parameter as Parameter
 from jack_server._server import Server as Server
+from jack_server._server import set_error_function as set_error_function
+from jack_server._server import set_info_function as set_info_function
