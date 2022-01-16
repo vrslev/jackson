f90b3cb9fd9bb5cdd1b54c8f394aa53386a0237a
Lev Vereshchagin <mail@vrslev.com>
2022-01-16 16:43:56 +0300

    Draft messaging client

---
+++ src/jackson/services/messaging/client.py
@@ -0,0 +1,19 @@
+import httpx
+
+from jackson.services.messaging.models import InitResponse
+
+
+class MessagingClient:
+    def __init__(self, base_url: str) -> None:
+        self.client = httpx.Client(base_url=base_url)
+
+    def init(self):
+        response = self.client.get("/init")
+        return InitResponse(**response.json())
+
+
+client = httpx.Client()
+
+
+def init():
+    ...
