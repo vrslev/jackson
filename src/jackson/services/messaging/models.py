2acf82db38b9edc55bdf61142ba1e943bc6af200
Lev Vereshchagin <mail@vrslev.com>
2022-01-16 16:43:46 +0300

    Rewrite messaging server

--- src/jackson/services/messaging/models.py
+++ src/jackson/services/messaging/models.py
@@ -1,6 +1,56 @@
+from typing import Any, Literal
+
+from fastapi import HTTPException, status
 from pydantic import BaseModel
 
 
 class InitResponse(BaseModel):
     inputs: int
     outputs: int
+
+
+class StructuredDetail(BaseModel):
+    message: str
+    data: BaseModel | None
+
+
+class StructuredHTTPException(HTTPException):
+    def __init__(
+        self,
+        status_code: int,
+        message: str,
+        data: BaseModel | None = None,
+        headers: dict[str, Any] | None = None,
+    ) -> None:
+        super().__init__(
+            status_code=status_code,
+            detail=StructuredDetail(message=message, data=data),
+            headers=headers,
+        )
+
+
+PortType = Literal["source", "destination"]
+
+
+class PortNotFound(BaseModel):
+    type: PortType
+    name: str
+
+    def exc(self):
+        return StructuredHTTPException(404, message="Source port not found", data=self)
+
+
+class PortAlreadyHasConnectionsData(BaseModel):
+    type: PortType
+    name: str
+    connection_names: list[str]
+
+    def exc(self):
+        return StructuredHTTPException(
+            status.HTTP_409_CONFLICT, message="Port already has connections", data=self
+        )
+
+
+class ConnectResponse(BaseModel):
+    source: str
+    destination: str
--- src/jackson/services/messaging/server.py
+++ src/jackson/services/messaging/server.py
@@ -1,33 +1,115 @@
-import jack
-from fastapi import FastAPI
+from typing import Literal
 
-from jackson.services.messaging.models import InitResponse
+import jack
+from fastapi import Depends, FastAPI, status
+
+from jackson.services.messaging.models import (
+    ConnectResponse,
+    InitResponse,
+    PortAlreadyHasConnectionsData,
+    PortNotFound,
+    PortType,
+    StructuredHTTPException,
+)
 
 app = FastAPI()
 
 
+async def get_jack_client():
+    return jack.Client("messaging-server")
+
+
 @app.get("/init", response_model=InitResponse)
-def init():
-    client = jack.Client("http-server")
+def init(client: jack.Client = Depends(get_jack_client)):
     inputs = client.get_ports("system:.*", is_input=True)
     outputs = client.get_ports("system:.*", is_output=True)
+
     return InitResponse(inputs=len(inputs), outputs=len(outputs))
 
 
-@app.get("/connect")
-def connect(source: str, destination: str):
-    s = source.startswith("system")
-    d = destination.startswith("system")
-    assert s or d
-    assert not (s and d)
+def check_client_name_not_system(client_name: str):
+    if client_name == "system":
+        raise StructuredHTTPException(
+            status.HTTP_400_BAD_REQUEST, 'Client name can\'t be "system"'
+        )
 
-    from_system = s
 
-    client = jack.Client("http-server")
+ServerShould = Literal["send", "receive"]
 
-    if from_system:
-        client.connect(source, destination)
+
+def resolve_ports_to_connect(
+    *,
+    server_should: ServerShould,
+    client_name: str,
+    server_port_number: int,
+    client_port_number: int,
+):
+    if server_should == "send":
+        source = f"system:capture_{server_port_number}"
+        destination = f"{client_name}:send_{client_port_number}"
     else:
-        port = client.get_port_by_name(destination)
-        if client.get_all_connections(port):
-            raise PermissionError("Somebody already sends information to this port.")
+        source = f"{client_name}:receive_{client_port_number}"
+        destination = f"system:playback_{server_port_number}"
+
+    return source, destination
+
+
+def get_port_or_raise(client: jack.Client, type: PortType, name: str):
+    try:
+        return client.get_port_by_name(name)
+    except jack.JackError:
+        raise PortNotFound(type=type, name=name).exc()
+
+
+def validate_port_has_no_connections(
+    client: jack.Client, port: jack.Port, type: PortType, name: str
+):
+    connections = client.get_all_connections(port)
+    if not connections:
+        return
+
+    raise PortAlreadyHasConnectionsData(
+        type=type, name=name, connection_names=[p.name for p in connections]
+    ).exc()
+
+
+@app.put("/connect", response_model=ConnectResponse)
+def connect(
+    *,
+    client: jack.Client = Depends(get_jack_client),
+    client_name: str,
+    server_should: ServerShould,
+    server_port_number: int,
+    client_port_number: int,
+):
+    check_client_name_not_system(client_name)
+    source_name, destination_name = resolve_ports_to_connect(
+        server_should=server_should,
+        client_name=client_name,
+        server_port_number=server_port_number,
+        client_port_number=client_port_number,
+    )
+
+    source = get_port_or_raise(client=client, type="source", name=source_name)
+    if server_should == "send":
+        # TODO: What is multiple clients connected....
+        validate_port_has_no_connections(
+            client=client, port=source, type="source", name=source_name
+        )
+
+    destination = get_port_or_raise(
+        client=client, type="destination", name=destination_name
+    )
+    if server_should == "receive":
+        validate_port_has_no_connections(
+            client=client, port=destination, type="destination", name=destination_name
+        )
+
+    client.connect(source_name, destination_name)
+    return ConnectResponse(source=source_name, destination=destination_name)
+
+
+if __name__ == "__main__":
+    import uvicorn  # type: ignore
+
+    uvicorn.run(app)  # type: ignore
