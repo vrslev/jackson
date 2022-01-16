b2dbb97202a05031235b37366151570a833d35d7
Lev Vereshchagin <mail@vrslev.com>
2022-01-16 20:16:12 +0300

    Add manager Client and Server classes to replace start_server and stop_server functions

--- src/jackson/main.py
+++ src/jackson/main.py
@@ -1,84 +1,9 @@
-import anyio
 import asyncer
 import typer
 from typer.params import Option
 
-import jack_server
-from jackson.services import jacktrip
-from jackson.services.util import generate_stream_handlers
+from jackson.manager import Client, Server
 from jackson.settings import ClientSettings, ServerSettings
-
-
-async def start_server(settings: ServerSettings):
-    info_stream_handler, error_stream_handler = generate_stream_handlers("jack")
-    jack = jack_server.Server(
-        driver=settings.audio.driver,
-        device=settings.audio.device,
-        rate=settings.audio.sample_rate,
-        info_stream_handler=info_stream_handler,
-        error_stream_handler=error_stream_handler,
-    )
-    # port_connector = PortConnector(settings.server.ports)
-
-    async with asyncer.create_task_group() as task_group:
-        try:
-            try:
-                jack.start()
-            except (
-                jack_server.ServerNotStartedError,
-                jack_server.ServerNotOpenedError,
-            ):
-                raise typer.Exit(1)
-            # port_connector.init()
-
-            # task_group.soonify(port_connector.start_queue)()
-            task_group.soonify(jacktrip.start_server)(port=settings.jacktrip_port)
-
-            while True:
-                await anyio.sleep(1)
-
-        finally:
-            with anyio.CancelScope(shield=True):
-                # port_connector.deinit()
-                jack.stop()
-
-
-async def start_client(settings: ClientSettings, start_jack: bool):
-    info_stream_handler, error_stream_handler = generate_stream_handlers("jack")
-    jack = jack_server.Server(
-        driver=settings.audio.driver,
-        device=settings.audio.device,
-        rate=48000,
-        # rate=settings.sample_rate, # TODO: Use sample rate and receive channels and send channels from /init
-        info_stream_handler=info_stream_handler,
-        error_stream_handler=error_stream_handler,
-    )
-    messaging_client = MessagingClient()
-    port_connector = PortConnector(settings.ports, settings.server.messaging_url)
-
-    async with asyncer.create_task_group() as task_group:
-        try:
-            if start_jack:
-                jack.start()
-            port_connector.init()
-
-            task_group.soonify(port_connector.start_queue)()
-            task_group.soonify(jacktrip.start_client)(
-                url=settings.server.jacktrip_url,
-                receive_channels=16,
-                send_channels=2,
-                remote_name=settings.name,
-            )
-
-            while True:
-                await anyio.sleep(1)
-
-        finally:
-            with anyio.CancelScope(shield=True):
-                port_connector.deinit()
-                if start_jack:
-                    jack.stop()
-
 
 app = typer.Typer()
 
@@ -86,7 +11,8 @@ app = typer.Typer()
 @app.command("server")
 def server_command(config: typer.FileText = Option("config.server.yaml")):
     settings = ServerSettings.from_yaml(config)
-    asyncer.runnify(start_server)(settings=settings)
+    server = Server(settings)
+    asyncer.runnify(server.run)()
 
 
 @app.command("client")
@@ -95,7 +21,8 @@ def client_command(
     no_jack: bool = Option(False, "--no-jack"),
 ):
     settings = ClientSettings.from_yaml(config)
-    asyncer.runnify(start_client)(settings=settings, start_jack=not no_jack)
+    client = Client(settings=settings, start_jack=not no_jack)
+    asyncer.runnify(client.run)()
 
 
 if __name__ == "__main__":
---
+++ src/jackson/manager.py
@@ -0,0 +1,97 @@
+from abc import ABC, abstractmethod
+
+import anyio
+import asyncer
+from asyncer._main import TaskGroup
+
+import jack_server
+from jackson.services import jacktrip
+from jackson.services.jack_server import JackServer
+from jackson.services.messaging.client import MessagingClient
+from jackson.services.port_connector import PortConnector
+from jackson.settings import ClientSettings, ServerSettings
+
+
+class _BaseManager(ABC):
+    @abstractmethod
+    async def start(self, task_group: TaskGroup):
+        ...
+
+    @abstractmethod
+    async def stop(self):
+        ...
+
+    async def run(self):
+        async with asyncer.create_task_group() as task_group:
+            try:
+                await self.start(task_group)
+            finally:
+                with anyio.CancelScope(shield=True):
+                    await self.stop()
+
+
+class Server(_BaseManager):
+    def __init__(self, settings: ServerSettings):
+        self.settings = settings
+        self.jack_server = JackServer(
+            driver=self.settings.audio.driver,
+            device=self.settings.audio.device,
+            rate=self.settings.audio.sample_rate,
+        )
+
+    async def start_jacktrip(self):
+        return await jacktrip.start_server(port=self.settings.server.jacktrip_port)
+
+    async def start(self, task_group: TaskGroup):
+        self.jack_server.start()
+        task_group.soonify(self.start_jacktrip)()
+
+    async def stop(self):
+        self.jack_server.stop()
+
+
+class Client(_BaseManager):
+    def __init__(self, settings: ClientSettings, start_jack: bool) -> None:
+        self.settings = settings
+        self.start_jack = start_jack
+        self.messaging_client = MessagingClient(
+            host=settings.server.host, port=settings.server.messaging_port
+        )
+        self.port_connector = PortConnector(settings.ports, self.messaging_client)
+
+    def start_jack_server(self, rate: jack_server.SampleRate):
+        self.jack_server = JackServer(
+            driver=self.settings.audio.driver,
+            device=self.settings.audio.device,
+            rate=rate,
+        )
+        self.jack_server.start()
+
+    async def start_jacktrip(self, receive_channels: int, send_channels: int):
+        return await jacktrip.start_client(
+            host=self.settings.server.host,
+            port=self.settings.server.jacktrip_port,
+            receive_channels=receive_channels,
+            send_channels=send_channels,
+            remote_name=self.settings.name,
+        )
+
+    async def start(self, task_group: TaskGroup):
+        init_response = await self.messaging_client.init()
+        if self.start_jack:
+            self.start_jack_server(init_response.rate)
+
+        self.port_connector.init()
+        task_group.soonify(self.port_connector.start_queue)()
+
+        task_group.soonify(self.start_jacktrip)(
+            receive_channels=init_response.outputs,
+            send_channels=init_response.inputs,  # TODO: This is not always true
+        )
+
+    async def stop(self):
+        await self.messaging_client.client.aclose()
+        self.port_connector.deinit()
+
+        if self.start_jack:
+            self.jack_server.stop()
