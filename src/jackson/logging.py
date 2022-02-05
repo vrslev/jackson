e24a5c4fdab38673950c42eec82d816ad1f1f3df
Lev Vereshchagin <mail@vrslev.com>
2022-02-05 14:01:55 +0300

    Improve logging

--- src/jackson/logging.py
+++ src/jackson/logging.py
@@ -51,33 +51,50 @@ def silent_jack_stream_handler(message: str):
 
 
 class JackClientFilter(logging.Filter):
+    messages = {
+        "CheckRes error",
+        "JackSocketClientChannel read fail",
+        "Cannot read socket fd = ",
+    }
+
     def filter(self, record: logging.LogRecord) -> bool:
-        if "CheckRes error" in record.msg:
-            return False
-        if "JackSocketClientChannel read fail" in record.msg:
-            return False
-        if "Cannot read socket fd = " and "err = Socket is not connected" in record.msg:
-            return False
+        for msg in self.messages:
+            if msg in record.msg:
+                return False
         return super().filter(record)
 
 
 class JackServerFilter(logging.Filter):
+    messages = {
+        "JackMachSemaphore::Destroy failed to kill semaphore",
+        "JackMachSemaphoreServer::Execute",
+        "self-connect-mode is",
+        "Input channel = ",
+        "JACK output port = ",
+        "CoreAudio driver is running...",
+    }
+
     def filter(self, record: logging.LogRecord) -> bool:
-        if "JackMachSemaphore::Destroy failed to kill semaphore" in record.msg:
-            return False
-        if "JackMachSemaphoreServer::Execute" in record.msg:
-            return False
+        for msg in self.messages:
+            if msg in record.msg:
+                return False
         return super().filter(record)
 
 
 class JackTripFilter(logging.Filter):
+    messages = {
+        "WEAK-JACK: initializing",
+        "WEAK-JACK: OK.",
+        "mThreadPool default maxThreadCount",
+        "mThreadPool maxThreadCount previously set",
+    }
+
     def filter(self, record: logging.LogRecord) -> bool:
-        if "WEAK-JACK: initializing" in record.msg:
+        for msg in self.messages:
+            if msg in record.msg:
+                return False
+
+        if all(c == "=" for c in record.msg):
             return False
-        if "WEAK-JACK: OK." in record.msg:
-            return False
-        if "mThreadPool default maxThreadCount" in record.msg:
-            return False
-        if "mThreadPool maxThreadCount previously set" in record.msg:
-            return False
+
         return super().filter(record)
