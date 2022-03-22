8d0b85eaa4f82ac61af7ae12795719c0cc7990d3
Lev Vereshchagin <mail@vrslev.com>
2022-03-22 12:19:32 +0300

    Refactor server port connector

--- src/jackson/connector/server.py
+++ src/jackson/connector/server.py
@@ -15,35 +15,6 @@ class PortConnectorError(Exception):
     data: BaseModel
 
 
-def init(client: jack.Client):
-    inputs = client.get_ports("system:.*", is_input=True)
-    outputs = client.get_ports("system:.*", is_output=True)
-
-    return models.InitResponse(
-        inputs=len(inputs),
-        outputs=len(outputs),
-        rate=cast(SampleRate, client.samplerate),
-        buffer_size=client.blocksize,
-    )
-
-
-def _get_existing_port(
-    client: jack.Client, type: models.PortDirectionType, name: PortName
-) -> jack.Port:
-    try:
-        return client.get_port_by_name(str(name))
-    except jack.JackError:
-        raise PortConnectorError(models.PortNotFound(type=type, name=name))
-
-
-def _get_ports_from_connection(
-    client: jack.Client, conn: models.Connection
-) -> tuple[jack.Port, jack.Port]:
-    source = _get_existing_port(client, type="source", name=conn.source)
-    destination = _get_existing_port(client, type="destination", name=conn.destination)
-    return source, destination
-
-
 def _validate_playback_port_is_free(
     name: PortName, connected_ports: list[jack.Port]
 ) -> None:
@@ -55,44 +26,60 @@ def _validate_playback_port_is_free(
     raise PortConnectorError(data)
 
 
-def _validate_connection(client: jack.Client, conn: models.Connection) -> None:
-    _, dest = _get_ports_from_connection(client, conn=conn)
-
-    if conn.client_should == "send":
-        _validate_playback_port_is_free(
-            name=conn.destination, connected_ports=client.get_all_connections(dest)
-        )
-
-
-async def _connect_ports(
-    client: jack.Client, source: PortName, destination: PortName
-) -> None:
-    try:
-        await connect_ports_retry(client, str(source), str(destination))
-    except jack.JackError:
-        raise PortConnectorError(
-            models.FailedToConnectPorts(source=source, destination=destination),
-        )
-
-
-async def connect(client: jack.Client, connections: list[models.Connection]):
-    for conn in connections:
-        _validate_connection(client, conn=conn)
-        await _connect_ports(client, conn.source, conn.destination)
-    return models.ConnectResponse()
-
-
 @dataclass
 class ServerPortConnector:
     client: jack.Client
 
     def init(self) -> models.InitResponse:
-        return init(self.client)
+        inputs = self.client.get_ports("system:.*", is_input=True)
+        outputs = self.client.get_ports("system:.*", is_output=True)
+
+        return models.InitResponse(
+            inputs=len(inputs),
+            outputs=len(outputs),
+            rate=cast(SampleRate, self.client.samplerate),
+            buffer_size=self.client.blocksize,
+        )
+
+    def _get_existing_port(
+        self, type: models.PortDirectionType, name: PortName
+    ) -> jack.Port:
+        try:
+            return self.client.get_port_by_name(str(name))
+        except jack.JackError:
+            raise PortConnectorError(models.PortNotFound(type=type, name=name))
+
+    def _get_ports_from_connection(
+        self, conn: models.Connection
+    ) -> tuple[jack.Port, jack.Port]:
+        source = self._get_existing_port(type="source", name=conn.source)
+        destination = self._get_existing_port(type="destination", name=conn.destination)
+        return source, destination
+
+    def _validate_connection(self, conn: models.Connection) -> None:
+        _, dest = self._get_ports_from_connection(conn)
+
+        if conn.client_should == "send":
+            _validate_playback_port_is_free(
+                name=conn.destination,
+                connected_ports=self.client.get_all_connections(dest),
+            )
+
+    async def _connect_ports(self, source: PortName, destination: PortName) -> None:
+        try:
+            await connect_ports_retry(self.client, str(source), str(destination))
+        except jack.JackError:
+            raise PortConnectorError(
+                models.FailedToConnectPorts(source=source, destination=destination),
+            )
 
     async def connect(
         self, connections: list[models.Connection]
     ) -> models.ConnectResponse:
-        return await connect(self.client, connections)
+        for conn in connections:
+            self._validate_connection(conn)
+            await self._connect_ports(conn.source, conn.destination)
+        return models.ConnectResponse()
 
     def close(self) -> None:
         block_jack_client_streams()
