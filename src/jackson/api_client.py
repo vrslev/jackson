f2a6161928c8d799cc87b2fbe4f0cb8d47a6920f
Lev Vereshchagin <mail@vrslev.com>
2022-03-22 15:29:52 +0300

    upd

--- src/jackson/api_client.py
+++ src/jackson/api_client.py
@@ -33,26 +33,29 @@ _known_errors: tuple[type[BaseModel], ...] = (
 )
 
 
+def _is_structured_exception(data: dict[str, Any]) -> bool:
+    return "message" in data["detail"] and "data" in data["detail"]
+
+
+def _find_model_by_name(name: str) -> type[BaseModel] | None:
+    for model in _known_errors:
+        if model.__name__ == name:
+            return model
+
+
 def _handle_exceptions(data: dict[str, Any]) -> None:
     if "detail" not in data:
         return
 
-    if "message" not in data["detail"]:
+    if not _is_structured_exception(data):
         raise RuntimeError(data)
 
-    message = data["detail"]["message"]
-    detail_data = data["detail"]["data"]
+    name = data["detail"]["message"]
 
-    model = None
-    for m in _known_errors:
-        if m.__name__ == message:
-            model = m
-            break
-
-    if model is None:
+    if not (model := _find_model_by_name(name)):
         raise RuntimeError(data)
 
-    raise ServerError(message=message, data=model(**detail_data))
+    raise ServerError(message=name, data=model(**data["detail"]["data"]))
 
 
 def _handle_response(response: httpx.Response) -> Any:
@@ -61,31 +64,28 @@ def _handle_response(response: httpx.Response) -> Any:
     return data
 
 
-async def _retry_request(
-    func: Callable[[], Coroutine[None, None, httpx.Response]],
-    times: int = 3,
-    delay: float = 0.5,
-) -> httpx.Response:
-    response = None
-
-    for _ in range(times):
-        response = await func()
-        if response.status_code == 404:
-            await anyio.sleep(delay)
-            continue
-        else:
-            return response
-
-    assert response
-    return response
-
-
 def _get_connections(map: ConnectionMap) -> Iterable[dict[str, Any]]:
     for connection in map.values():
         src, dest = connection.get_remote_connection()
         yield Connection(
             source=src, destination=dest, client_should=connection.client_should
         ).dict()
+
+
+async def _retry_connect(
+    func: Callable[[], Coroutine[None, None, httpx.Response]]
+) -> httpx.Response:
+    response = None
+
+    for _ in range(3):
+        response = await func()
+        if response.status_code == 404:
+            await anyio.sleep(0.5)
+        else:
+            return response
+
+    assert response
+    return response
 
 
 @dataclass
@@ -99,6 +99,5 @@ class APIClient:
     async def connect(self, connection_map: ConnectionMap) -> None:
         payload = list(_get_connections(connection_map))
         func = partial(self.client.patch, "/connect", json=payload)  # type: ignore
-
-        response = await _retry_request(func)
+        response = await _retry_connect(func)
         ConnectResponse(**_handle_response(response))
