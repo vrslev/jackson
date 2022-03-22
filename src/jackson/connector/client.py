564fe2a522c17d73f938241c83ec639cfebee6fd
Lev Vereshchagin <mail@vrslev.com>
2022-03-22 12:02:50 +0300

    Pass jack client to client port connector

--- src/jackson/connector/client.py
+++ src/jackson/connector/client.py
@@ -4,11 +4,7 @@ from typing import Callable, Coroutine
 import anyio
 import jack
 
-from jackson.jack_client import (
-    block_jack_client_streams,
-    connect_ports_retry,
-    init_jack_client,
-)
+from jackson.jack_client import block_jack_client_streams, connect_ports_retry
 from jackson.port_connection import ConnectionMap
 
 ConnectOnServer = Callable[[ConnectionMap], Coroutine[None, None, None]]
@@ -16,17 +12,13 @@ ConnectOnServer = Callable[[ConnectionMap], Coroutine[None, None, None]]
 
 @dataclass
 class ClientPortConnector:
-    jack_server_name: str
+    client: jack.Client
     connection_map: ConnectionMap
     connect_on_server: ConnectOnServer
     ready: anyio.Event = field(default_factory=anyio.Event, init=False)
     client_activated: bool = field(default=False, init=False)
-    client: jack.Client = field(init=False)
 
     def __post_init__(self) -> None:
-        self.client = init_jack_client(
-            "PortConnector", server_name=self.jack_server_name
-        )
         self.client.set_client_registration_callback(self.client_registration_callback)
         self.client.activate()
         self.client_activated = True
