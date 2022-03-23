2e94527ddb08cb84fb5fe0107b776f144c25375e
Lev Vereshchagin <mail@vrslev.com>
2022-03-23 10:05:29 +0300

    Add tests for jack_server module

--- src/jackson/jack_server.py
+++ src/jackson/jack_server.py
@@ -18,13 +18,9 @@ log = get_logger(__name__, "JackServer")
 log.addFilter(JackServerFilter())
 
 
-def _set_stream_handlers() -> None:
+def start_jack_server(server: jack_server.Server) -> None:
     jack_server.set_info_function(log.info)
     jack_server.set_error_function(log.error)
-
-
-def start_jack_server(server: jack_server.Server) -> None:
-    _set_stream_handlers()
     server.start()
 
 
