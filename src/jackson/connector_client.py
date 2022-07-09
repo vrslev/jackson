99ae757f63ae8c3b37a05da74e29c6a3c49cda81
Lev Vereshchagin <mail@vrslev.com>
2022-07-09 15:16:58 +0300

    u

--- src/jackson/connector_client.py
+++ src/jackson/connector_client.py
@@ -10,25 +10,20 @@ from jackson.port_connection import ConnectionMap
 
 async def connect_server_and_client_ports(
     client: jack.Client,
-    connection_map: ConnectionMap,
     connect_on_server: Callable[[ConnectionMap], Coroutine[None, None, None]],
+    connection_map: ConnectionMap,
 ) -> None:
-    def cb(name: str, register: bool) -> None:
+    def on_register(name: str, register: bool) -> None:
         if register and name == JACK_CLIENT_NAME:
             ready.set()
 
     ready = anyio.Event()
-    client.set_client_registration_callback(cb)
+    client.set_client_registration_callback(on_register)
     client.activate()
+
     await ready.wait()
-
-    def connect_locally() -> None:
-        for conn in connection_map.values():
-            src, dest = conn.get_local_connection()
-            connect_ports_safe(client, str(src), str(dest))
-
     await connect_on_server(connection_map)
-    connect_locally()
-    # async with anyio.create_task_group() as tg:
-    #     tg.start_soon(lambda: connect_on_server(connection_map))
-    #     tg.start_soon(connect_locally)
+
+    for conn in connection_map.values():
+        src, dest = conn.get_local_connection()
+        connect_ports_safe(client, str(src), str(dest))
